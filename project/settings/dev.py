e0e48b2d5a40e100de215e7d92085c32c9385ba2
Alex Clark <aclark@aclark.net>
2022-03-05 12:28:21 -0500

    Update

---
+++ project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3*5bq6t1lcbpg*=mh^s##x35e$ne$_kkym1gj+!$!v0p^37+7d'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
