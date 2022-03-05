3f770439f3760d3b44d6d42c5be02e33177034ba
Alex Clark <aclark@aclark.net>
2022-03-05 09:38:55 -0500

    Update

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yt$ltyi-&t+99n(3rlfk56=gkzur5=-ft+(2ew%tm09-59gd@_'
+SECRET_KEY = 'django-insecure--s)9pskttdo)pk%7=zo&65ey8!5$6b=^p@2g(^nq$6r53u)*bn'
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
